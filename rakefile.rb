6a706cf965f70385f314446beeb9deab306b1c09
Rune Lind <rulind@gmail.com>
2013-07-19 21:25:39 +0200

    updated rakefile

--- rakefile.rb
+++ rakefile.rb
@@ -1,15 +1,19 @@
 require "rspec/core/rake_task"
+require_relative 'lib/run_backup'
 
 desc "Run RSpec unit tests"
 RSpec::Core::RakeTask.new(:spec) do |t|
   t.pattern       = 'spec/*_spec.rb'
 end
 
-desc "desc"
-task :solr_bench, [:comp_cnt,:thread_cnt,:servers] do |t, args|
+desc "back hdfs to s3"
+task :backup, [:hdfs_path, :s3_dir] do |t, args|
   args.with_defaults(
-    :comp_cnt => 10
+    :hdfs_path => '/user/ben/BACKUP',
+    :s3_dir => 'BACKUP-FULL-TAKE-4-2013.06.10',
   )
+  p args
+  RunBackup.new.start_backup(args[:hdfs_path], args[:s3_dir])
 end
 
 
