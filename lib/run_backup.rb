d41b18cf34ab04150724fc3f5aaf5c502be379fa
Rune Lind <rulind@gmail.com>
2013-07-23 09:57:00 +0200

    code refactoring

--- lib/run_backup.rb
+++ lib/run_backup.rb
@@ -7,37 +7,33 @@ class RunBackup
     mkdir('hdfs-to-s3')
 
     hdfs_tables = get_catalogs_with_size(hdfs_path)
-
     total_size = get_size_from_folders(hdfs_tables)
-    msg = "size #{hdfs_path} in #{hdfs_tables.length} table-folders is #{total_size}"
-    log.info msg
+    report_status "processing root folder '#{hdfs_path}' with #{hdfs_tables.length} folders. Total size=#{total_size}"
 
     hdfs_tables.each do |table_path, size|
       table_name = get_last_name_from_path(table_path)
-      puts "table'#{table_path}' size: #{size}"
-      log.info "table'#{table_path}' size: #{size}"
-      mkdir("hdfs-to-s3/#{table_name}")
+      report_status "processing:'#{table_path}' size: #{size}"
 
-      get_catalogs_with_size(table_path).each do |hdfs_file_path, file_size|
-        file_name = get_last_name_from_path(hdfs_file_path)
-        log.info "hdsf_file '#{hdfs_file_path}' size:#{file_size}"
-        log.info "file_name:''#{file_name}"
-        copy_local(hdfs_file_path, file_name, file_size, table_name) do |local_file, folder|
-          transfer_to_s3(local_file, folder, s3_dir)
-        end
-      end
-      shell_cmd "rm -rf hdfs-to-s3/#{table_name}"
+      process_sub_folders(s3_dir, table_name, table_path)
     end
   end
 
-  def mkdir(dir)
-    shell_cmd("mkdir -p #{dir}")
+  def process_sub_folders(s3_dir, table_name, table_path)
+    mkdir("hdfs-to-s3/#{table_name}")
+    get_catalogs_with_size(table_path).each do |hdfs_file_path, file_size|
+      file_name = get_last_name_from_path(hdfs_file_path)
+      log.info "hdsf_file '#{hdfs_file_path}' size:#{file_size}"
+      copy_local(hdfs_file_path, file_name, file_size, table_name) do |local_file, folder|
+        transfer_to_s3(local_file, folder, s3_dir)
+      end
+    end
+    rm_dir("hdfs-to-s3/#{table_name}")
   end
 
   def copy_local(hdfs_file_path, file_name, file_size, folder, &block)
     shell_cmd("hadoop fs -copyToLocal #{hdfs_file_path} hdfs-to-s3/#{folder}")
     block.call(file_name, folder)
-    shell_cmd "rm -f hdfs-to-s3/#{folder}/#{file_name}"
+    rm_file "hdfs-to-s3/#{folder}/#{file_name}"
   end
 
   def transfer_to_s3(file, folder, s3_dir)
@@ -53,8 +49,6 @@ class RunBackup
   end
 
   def get_catalogs_with_size(path='')
-    puts "getting table_path folders of #{path}"
-    log.info "getting table_path folders of #{path}"
     ret_code, files = shell_cmd("hadoop fs -du #{path}")
     files.map { |line| line.split(/\s+/).reverse }.sort_by { |folder, size| size }
   end
@@ -73,14 +67,13 @@ class RunBackup
     result = %x[#{cmd}]
     result = result.split("\n") if result
     exitstatus = $?.exitstatus
-    log.info "cmd = '#{cmd}' exit:#{exitstatus} time:#{'%.1f' % (Time.now-start)}"
+    log.info "cmd:'#{cmd}' exit:#{exitstatus} time:#{'%.1f' % (Time.now-start)}"
     return exitstatus, result
   end
 
   def with_retry(retry_cnt, msg, &code)
     (1..retry_cnt).each do |i|
       r_code = code.call()
-      log.info "return code= #{r_code}"
       return if r_code == 0
       sleep_time = 10*i
       log.warn msg
@@ -90,4 +83,20 @@ class RunBackup
     raise "retry #{retry_cnt} failed: #{msg}"
   end
 
+  def mkdir(dir)
+    shell_cmd("mkdir -p #{dir}")
+  end
+
+  def rm_dir(path)
+    shell_cmd "rm -rf #{path}"
+  end
+
+  def rm_file(path)
+    shell_cmd "rm -f #{path}"
+  end
+
+  def report_status(msg)
+    puts msg
+    log.info msg
+  end
 end
